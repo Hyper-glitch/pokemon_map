1a09839ad311879e29bfc2484320eba103853f69
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-30 16:58:56 +0300

    feature:(field) add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, null=True, upload_to='pokemons')
 
     def __str__(self):
         return self.title
