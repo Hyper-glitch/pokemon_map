0c61195299f56c5be60522159ccf22c17b2b58f4
Roman Smirnov <r.smirnov@okenit.ru>
2022-06-30 17:18:38 +0300

    feature:(admin) register PokemonGeo model

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Pokemon
+from .models import Pokemon, PokemonGeo
 
 
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PokemonGeo)
+class PokemonGeoAdmin(admin.ModelAdmin):
     pass
