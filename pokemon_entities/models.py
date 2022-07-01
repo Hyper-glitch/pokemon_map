7c94f835d624dd523f3f5a90c970a94a8e7e4ba4
Roman Smirnov <r.smirnov@okenit.ru>
2022-07-01 15:31:01 +0300

    feature:(fields) add specs to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -13,8 +13,13 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
     latitude = models.FloatField(max_length=6, verbose_name=' Широта')
     longitude = models.FloatField(max_length=6, verbose_name=' Долгота')
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
+    appeared_at = models.DateTimeField(verbose_name='Появился в')
+    disappeared_at = models.DateTimeField(verbose_name='Исчез в')
+    level = models.PositiveSmallIntegerField(default=1, verbose_name='Уровень')
+    health = models.PositiveSmallIntegerField(default=100, verbose_name='Здоровье')
+    strength = models.PositiveSmallIntegerField(default=10, verbose_name='Атака')
+    defence = models.PositiveSmallIntegerField(default=10, verbose_name='Защита')
+    stamina = models.PositiveSmallIntegerField(default=10, verbose_name='Выносливость')
 
     def __str__(self):
         return self.pokemon.title
