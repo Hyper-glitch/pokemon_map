b459fb2a6cfad8a8230632e6dcbc013d31d8129c
Roman <graff049@gmail.com>
2022-07-03 20:16:49 +0300

    feature: move all helpful tools to a new module

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,34 +1,17 @@
-import json
-
 import folium
-from django.db.models import Q
-from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.utils.timezone import localtime
 
-from pokemon_entities.models import PokemonEntity, Pokemon
+from pokemon_entities.models import Pokemon
+from pokemon_entities.show_pokemons_tools import show_pokemons_on_map
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 
 
-def add_pokemon_to_map(map, entity, url):
-    icon = folium.features.CustomIcon(url, icon_size=(50, 50))
-    # Warning! `tooltip` attribute is disabled intentionally to fix strange folium cyrillic encoding bug
-    folium.Marker([entity.latitude, entity.longitude], icon=icon).add_to(map)
-
-
 def show_all_pokemons(request):
-    now = localtime()
-    actual_pokemon_entities = PokemonEntity.objects.filter(Q(appeared_at__lt=now) & Q(disappeared_at__gt=now))
-
     serialized_pokemons = []
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     pokemons = Pokemon.objects.all()
-
-    map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-
-    for entity in actual_pokemon_entities:
-        image_url = request.build_absolute_uri(entity.pokemon.image.url)
-        add_pokemon_to_map(map=map, entity=entity, url=image_url)
+    show_pokemons_on_map(request, folium_map)
 
     for pokemon in pokemons:
         image_url = request.build_absolute_uri(pokemon.image.url)
@@ -41,7 +24,7 @@ def show_all_pokemons(request):
         )
 
     context = {
-        'map': map._repr_html_(),
+        'map': folium_map._repr_html_(),
         'pokemons': serialized_pokemons,
     }
 
@@ -49,22 +32,13 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    for pokemon in pokemons:
-        if pokemon['pokemon_id'] == int(pokemon_id):
-            requested_pokemon = pokemon
-            break
-    else:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
-
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon_entity in requested_pokemon['entities']:
-        add_pokemon_to_map(
-            folium_map, pokemon_entity['lat'],
-            pokemon_entity['lon'],
-            pokemon['img_url']
-        )
-    context = {'map': folium_map._repr_html_(), 'pokemon': pokemon}
+    show_pokemons_on_map(request, folium_map)
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    image_url = request.build_absolute_uri(pokemon.image.url)
+    serialized_pokemon = {
+        'img_url': image_url,
+        'title_ru': pokemon.title,
+    }
+    context = {'map': folium_map._repr_html_(), 'pokemon': serialized_pokemon}
     return render(request, 'pokemon.html', context=context)
