52b6ec0b6ef6dd8956f5d621732049364e798d7f
Roman <graff049@gmail.com>
2022-07-07 12:47:14 +0300

    feature(docstring): add a docstring refactor(map): init folium_map just in function

--- pokemon_entities/show_pokemons_tools.py
+++ pokemon_entities/show_pokemons_tools.py
@@ -1,20 +1,27 @@
+"""Module that helps add and show pokemons to a map."""
 import folium
-from django.db.models import Q
-from django.utils.timezone import localtime
 
-from pokemon_entities.models import PokemonEntity
+MOSCOW_CENTER = [55.751244, 37.618423]
 
 
-def add_pokemon_to_map(map, entity, url):
+def add_pokemon_to_map(entity, url):
+    """Add pokemon entity to a map.
+    :param entity: pokemon's entity obj from database.
+    :param url: absolute url for pokemon img.
+    :return: None
+    """
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     icon = folium.features.CustomIcon(url, icon_size=(50, 50))
     # Warning! `tooltip` attribute is disabled intentionally to fix strange folium cyrillic encoding bug
-    folium.Marker([entity.latitude, entity.longitude], icon=icon).add_to(map)
+    folium.Marker([entity.latitude, entity.longitude], icon=icon).add_to(folium_map)
 
 
-def show_pokemons_on_map(request, folium_map):
-    now = localtime()
-    actual_pokemon_entities = PokemonEntity.objects.filter(Q(appeared_at__lt=now) & Q(disappeared_at__gt=now))
+def show_pokemons_on_map(request, entity):
+    """
 
-    for entity in actual_pokemon_entities:
-        image_url = request.build_absolute_uri(entity.pokemon.image.url)
-        add_pokemon_to_map(map=folium_map, entity=entity, url=image_url)
+    :param request:
+    :param entity:
+    :return:
+    """
+    image_url = request.build_absolute_uri(entity.pokemon.image.url)
+    add_pokemon_to_map(entity=entity, url=image_url)
