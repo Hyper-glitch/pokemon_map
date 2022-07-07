1e233ece0a71bc8236eb96ee3ecc4fe0da53d5dc
Roman <graff049@gmail.com>
2022-07-07 14:42:07 +0300

    refactor(common): make refactoring greate again

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,20 +1,18 @@
 import folium
 from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
-from django.db.models import Q
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.utils.timezone import localtime
 
-from pokemon_entities.models import Pokemon, PokemonEntity
-from pokemon_entities.show_pokemons_tools import show_pokemon_on_map, MOSCOW_CENTER
+from pokemon_entities.models import Pokemon
+from pokemon_entities.show_pokemons_tools import show_pokemon_on_map, MOSCOW_CENTER, get_actual_pokemons, \
+    serialize_pokemon
 
 
 def show_all_pokemons(request):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     serialized_pokemons = []
     pokemons = Pokemon.objects.all()
-    now = localtime()
-    actual_pokemon_entities = PokemonEntity.objects.filter(Q(appeared_at__lt=now) & Q(disappeared_at__gt=now))
+    actual_pokemon_entities = get_actual_pokemons()
 
     for entity in actual_pokemon_entities:
         show_pokemon_on_map(request=request, entity=entity, folium_map=folium_map)
@@ -39,49 +37,16 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    previous_evolution = None
-    serialized_next_evolution = None
-    now = localtime()
-    actual_pokemon_entities = PokemonEntity.objects.filter(pokemon__id=pokemon_id).filter(
-        Q(appeared_at__lt=now) & Q(disappeared_at__gt=now))
+    actual_pokemon_entities = get_actual_pokemons(pokemon_id=pokemon_id, show_all=False)
 
     for entity in actual_pokemon_entities:
         show_pokemon_on_map(request=request, entity=entity, folium_map=folium_map)
-
     try:
         pokemon = Pokemon.objects.get(id=pokemon_id)
     except (MultipleObjectsReturned, ObjectDoesNotExist):
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
 
-    next_evolution_entity = PokemonEntity.objects.filter(pokemon__previous_evolution=pokemon_id).first()
-
-    if next_evolution_entity:
-        next_evolution = next_evolution_entity.pokemon
-    else:
-        next_evolution = None
-
-    if pokemon.previous_evolution:
-        previous_evolution = {
-            'title_ru': pokemon.previous_evolution.title_ru,
-            'pokemon_id': pokemon.previous_evolution.id,
-            'img_url': request.build_absolute_uri(pokemon.previous_evolution.image.url),
-        }
-    if next_evolution:
-        serialized_next_evolution = {
-            'title_ru': next_evolution.title_ru,
-            'pokemon_id': next_evolution.id,
-            'img_url': request.build_absolute_uri(next_evolution.image.url),
-        }
-
-    image_url = request.build_absolute_uri(pokemon.image.url)
-    serialized_pokemon = {
-        'img_url': image_url,
-        'title_ru': pokemon.title_ru,
-        'title_en': pokemon.title_en,
-        'title_jp': pokemon.title_jp,
-        'description': pokemon.description,
-        'previous_evolution': previous_evolution,
-        'next_evolution': serialized_next_evolution,
-    }
+    serialized_pokemon = serialize_pokemon(request=request, pokemon=pokemon, pokemon_id=pokemon_id)
     context = {'map': folium_map._repr_html_(), 'pokemon': serialized_pokemon}
+
     return render(request, 'pokemon.html', context=context)
